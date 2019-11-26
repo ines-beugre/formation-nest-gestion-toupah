02f7c86c41c49cd2b8fc7a9338069b51d7a92905
ines-beugre.niji <ines.beugre@niji.fr>
2019-11-26 14:21:39 +0100

    ajout de person controller

--- src/entity/person.ts
+++ src/entity/person.ts
@@ -1,4 +1,4 @@
-export interface Person {
+export class Person {
     lastname: string;
     firstname: string;
     generation: string; 
--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,10 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put } from '@nestjs/common';
+import { CreatePersonDto, UpdatePersonDto } from '../dto/person-dto';
+import { PersonService } from './person.service';
+import { Person } from 'src/entity/person';
 
 @Controller('person')
 export class PersonController {
+    personService: PersonService;
 
     @Get()
     TestPerson() {
         return 'Person App';
     }
+
+    @Get()
+    async findAll(): Promise<Person[]> {
+        return this.personService.findAll();
+    }
+    
+    @Post()
+    async create(@Body() CreatePersonDto: CreatePersonDto) {
+       this.personService.create(CreatePersonDto) ;
+    }
+
+    @Get('/hello/:name')
+    getPersonalHello(@Param('name') name: string) {
+        return this.personService.getPersonalHello(name);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
+        return this.personService.update(id, updatePersonDto);
+    }
+    
 }
